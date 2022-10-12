26991556f54324b52c7cc435e45467dea87843d1
Dedo-unuka <dzanazovko@gmail.com>
2022-10-12 10:35:42 +0200

    ex04

--- ex04/react-app/src/index.js
+++ ex04/react-app/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM  from 'react-dom';
 import './index.css';
 
 
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello, Padawans!</h1>
+                <h1>Hello, React!</h1>
             </div>
         )
     }
