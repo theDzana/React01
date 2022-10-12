c7c699544fe975f14bf6c2f4e80044609a7761a5
Dedo-unuka <dzanazovko@gmail.com>
2022-10-12 11:20:20 +0200

    React01 day 1

---
+++ ex06/react-app/src/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import './index.css';
+
+
+class App extends React.Component {
+    render() {
+      const world="World";
+        return (
+            <div className="App">
+                <h1>Hello, {world}!</h1>
+            </div>
+        )
+    }
+}
+export default App;
+
+ ReactDOM.render(<App />, document.getElementById('root'));
